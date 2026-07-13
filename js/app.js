92b25044828ab1dfbef7a4f3e55e958e3d1330ed
gotigiuszek009-sudo <gotigiuszek009@gmail.com>
2026-07-13 22:02:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 
 const popup = document.getElementById("popup");
 const openBtn = document.getElementById("openPopup");
-const closeBtn = document.querySelector(".close");
 
-openBtn.onclick = () => popup.style.display = "flex";
 closeBtn.onclick = () => popup.style.display = "none";
 
 window.onclick = (e) => {
