c3a5958e12fe7e3f2ff2e7747434b3640bd5492d
gotigiuszek009-sudo <gotigiuszek009@gmail.com>
2026-07-13 20:37:39 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,104 @@
+let cart=[];
+
+
+// KOSZYK
+
+function addToCart(name,price){
+
+cart.push({
+name:name,
+price:price
+});
+
+updateCart();
+
+}
+
+
+
+function updateCart(){
+
+document.getElementById("cart-count").innerHTML=cart.length;
+
+
+let items=document.getElementById("cart-items");
+
+items.innerHTML="";
+
+
+let total=0;
+
+
+cart.forEach((item,index)=>{
+
+total+=item.price;
+
+
+items.innerHTML+=`
+
+<p>
+${item.name} - ${item.price} zł
+
+<button onclick="removeCart(${index})">
+❌
+</button>
+
+</p>
+
+`;
+
+});
+
+
+document.getElementById("cart-total").innerHTML=total;
+
+}
+
+
+
+function removeCart(index){
+
+cart.splice(index,1);
+
+updateCart();
+
+}
+
+
+
+function openCart(){
+
+document.getElementById("cart-window").style.display="block";
+
+}
+
+
+
+function closeCart(){
+
+document.getElementById("cart-window").style.display="none";
+
+}
+
+
+
+
+// TRYB CIEMNY
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+const button=document.getElementById("theme-toggle");
+
+
+button.addEventListener("click",()=>{
+
+
+document.body.classList.toggle("dark-mode");
+
+
+});
+
+
+});
